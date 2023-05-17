a5ec7929be413104b08d43195e12b8db6bb5beb7
Sandra Quispe <sandra.quispe.f@tecsup.edu.pe>
2023-05-17 09:12:07 -0500

    Initial Commit

---
+++ PaucarColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaucarColeccionDeJuegos
+//
+//  Created by Mac 10 on 17/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
